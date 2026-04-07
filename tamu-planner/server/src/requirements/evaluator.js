58d132b77847b161be8804567b720c1ee0f2071c
raymaredia28 <raymaredia28@gmail.com>
2026-04-07 09:16:29 -0500

    Additional bug fixes to UI and difficulty ranking

--- tamu-planner/server/src/requirements/evaluator.js
+++ tamu-planner/server/src/requirements/evaluator.js
@@ -675,10 +675,14 @@ const summarizeGroup = (name, rules, context, { deriveCredits = false } = {}) =>
   };
 };
 
-const computeWorkNotApplied = (groups, courseIndex) => {
+const computeWorkNotApplied = (groups, courseIndex, externallyAppliedCodes = []) => {
   const allUsed = new Set();
   groups.forEach(group => {
     (group.usedCourses || []).forEach(code => allUsed.add(code));
+  });
+  externallyAppliedCodes.forEach((code) => {
+    const normalized = normalizeCode(code);
+    if (normalized) allUsed.add(normalized);
   });
 
   const seenRecords = new Set();
@@ -770,7 +774,14 @@ const findAlternativePlacements = (unappliedCourses, requirementSet, courseIndex
   });
 };
 
-const evaluateRequirements = ({ requirementSet, studentCourses, emphasisCourseIds, hasHsLanguage, hasSabrCourse }) => {
+const evaluateRequirements = ({
+  requirementSet,
+  studentCourses,
+  emphasisCourseIds,
+  hasHsLanguage,
+  hasSabrCourse,
+  externallyAppliedCodes = []
+}) => {
   const courseIndex = buildCourseIndex(studentCourses);
   const completedCourses = Array.from(courseIndex.values()).filter((course) =>
     isCompleted(course, null)
@@ -789,7 +800,7 @@ const evaluateRequirements = ({ requirementSet, studentCourses, emphasisCourseId
     summarizeGroup(group.name, group.rules || {}, context, { deriveCredits: isMinor })
   );
 
-  let workNotApplied = computeWorkNotApplied(groups, courseIndex);
+  let workNotApplied = computeWorkNotApplied(groups, courseIndex, externallyAppliedCodes);
   workNotApplied = findAlternativePlacements(workNotApplied, requirementSet, courseIndex);
 
   return {
